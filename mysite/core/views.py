f556ed326969ca1b19992a1c664fdc7e3f4d45df
Shweta <shwetadorle2022@gmail.com>
2026-01-27 14:45:23 +0530

    Writing Django response

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
 """Where request logic lives
-
 Functions or classes that return responses"""
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+'''Let’s break this down:
+Request
+An object containing:
+    HTTP method (GET/POST)
+    headers
+    user data
+HttpResponse
+  Django’s way of sending text back to the browser
+
+This function:
+  takes a request
+  returns a response
+That’s a Django view'''
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
+
+'''
+Means:
+URL: /
+    Call function: home
+    Return its response
+So now:
+    Browser → "/" → urls.py → home() → HttpResponse → Browser
+'''
