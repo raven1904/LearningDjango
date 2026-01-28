b8e13e601d5ff7335a3c87b94fcd4f918da7a9d7
Shweta <shwetadorle2022@gmail.com>
2026-01-28 22:19:39 +0530

    Models and ORM

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-'''Database tables (ORM)
+'''Database tables (ORM), Each class = one table'''
 
-Each class = one table'''
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
